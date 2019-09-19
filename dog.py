ffa3fea789bc9ab6001e5a957cecc56bb74395d7
= <zainr7989@gmail.com>
2019-09-19 09:28:03 -0700

    Add instance method bark().

--- dog.py
+++ dog.py
@@ -1,10 +1,14 @@
 # page one of the tutorial
 class Dog:
+    # required properties go inside constructor
     def __init__(self, name, breed):
         self.name = name
         self.breed = breed
 
+    # named functions inside class = methods
+    def bark(self):
+        print("Woof!")
 
-my_dog = Dog("Tommy")
-print(my_dog)
-print(my_dog.breed)
+
+my_dog = Dog("Tommy", "WatchDog")
+my_dog.bark()
