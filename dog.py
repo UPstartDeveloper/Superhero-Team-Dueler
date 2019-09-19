c7c0d98a29027d3a9bc9b6cd13d0e970a2404e30
= <zainr7989@gmail.com>
2019-09-19 09:22:14 -0700

    Trigger TypeError by leaving out required argument at instantiation.

--- dog.py
+++ dog.py
@@ -1,9 +1,10 @@
 # page one of the tutorial
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, breed):
         self.name = name
+        self.breed = breed
 
 
 my_dog = Dog("Tommy")
 print(my_dog)
-print(my_dog.name)
+print(my_dog.breed)
